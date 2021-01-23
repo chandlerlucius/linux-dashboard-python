74fed991e224025500b8e0521481c5210ae5af23
Chandler Lucius <clucius@chandlerlucius.com>
2021-01-23 17:31:06 -0600

    Added query info from db method

--- server/transaction.py
+++ server/transaction.py
@@ -11,6 +11,12 @@ class Transaction:
         self.db_client = InfluxDBClient(host='localhost', port=8086, database='system_stats')
         self.db_client.create_database('system_stats')
 
+    async def query_info_from_db(self, table, fields):
+        query = 'SELECT ' + ','.join(fields) + ' FROM ' + table + ' WHERE time > now() - ' + Transaction.TIME_PERIOD
+        data = self.db_client.query(query) 
+        data.raw['series'][0]['type'] = 'info'
+        return json.dumps(data.raw['series'][0])
+
     async def query_usage_from_db(self, table, fields):
         query = 'SELECT ' + ','.join(fields) + ' FROM ' + table + ' WHERE time > now() - ' + Transaction.TIME_PERIOD
         data = self.db_client.query(query) 
