e71de53f42bc3f81390b87e8e4acb180fbe4b827
Chandler Lucius <clucius@chandlerlucius.com>
2021-02-09 23:25:53 -0600

    Restored old version of code where wait was exactly 1 second

--- server/websocket.py
+++ server/websocket.py
@@ -10,6 +10,28 @@ import json
 import time
 
 clients = set()
+    
+# async def calculate_and_store_disk_usage(parameter):
+#     io_millis = run_script('disk_stats')
+#     curr_io_millis = io_millis['io_millis']
+    
+#     prev_io_millis = await transaction.query_last_record_from_db('disk')
+#     prev_millis = prev_io_millis['io_millis'] if 'io_millis' in prev_io_millis else 0
+    
+#     curr_time = datetime.utcnow()
+#     prev_time = datetime.strptime(prev_io_millis['time'], '%Y-%m-%dT%H:%M:%SZ') if 'time' in prev_io_millis else datetime.utcnow()
+
+#     disk_usage = (curr_io_millis - prev_millis) * 100 / ((curr_time - prev_time).total_seconds() * 1000)
+
+#     data = [{
+#         "measurement" : "disk",
+#         "time": curr_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
+#         "fields": {
+#             "io_millis": curr_io_millis,
+#             "disk_usage": disk_usage
+#         }
+#     }]
+#     await transaction.write_to_db(data)
 
 def run_script(data_type):
     script_output = check_output(['sh', 'server_stats.sh', data_type]).decode("utf-8")
@@ -33,25 +55,21 @@ loop.run_until_complete(start_server)
 async def get_and_send_data_async(json):
     while True:
         try:
-            before=time.time()
             data = run_script(json["id"])
             await send_to_clients(data)
         except Exception:
             print(traceback.format_exc())
         finally:
-            after=time.time()
-            await asyncio.sleep(json["interval"] - (after - before))
+            await asyncio.sleep(json["interval"])
 
 async def store_data(json):
     while True:
         try:
-            before=time.time()
             run_script(json["id"])
         except Exception:
             print(traceback.format_exc())
         finally:
-            after=time.time()
-            await asyncio.sleep(json["interval"] - (after - before))
+            await asyncio.sleep(json["interval"])
 
 def get_categories_and_run_in_intervals():
     categories = run_script("get_categories")
